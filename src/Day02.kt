72674b7eb8ce2a34f8510edd7da2d89699fb60f2
enzosarmento <enzosarmento91@gmail.com>
2024-12-04 00:50:50 -0300

    Day02 part2

--- src/Day02.kt
+++ src/Day02.kt
@@ -23,7 +23,51 @@ fun main() {
         return answer
     }
 
+    fun part2(input: List<String>): Int {
+        var answer = 0
+        for (i in input.indices) {
+            val line = input[i].split(" ")
+            val lineInt = line.map { it.toInt() }.toMutableList()
+            var increasing = 0
+            var decreasing = 0
+
+            for (j in 1..<lineInt.size) {
+                val diference = abs(lineInt[j] - lineInt[j - 1])
+                if (lineInt[j - 1] < lineInt[j] && diference <= 3) {
+                    increasing++
+                } else if (lineInt[j - 1] > lineInt[j] && diference <= 3) {
+                    decreasing++
+                }
+            }
+            if (increasing == lineInt.size - 1 || decreasing == lineInt.size - 1) answer++
+            else {
+                increasing = 0
+                decreasing = 0
+                for (j in lineInt.indices) {
+                    val value = lineInt.removeAt(j)
+                    for (k in 1..<lineInt.size) {
+                        val diference = abs(lineInt[k] - lineInt[k - 1])
+                        if (lineInt[k - 1] < lineInt[k] && diference <= 3) {
+                            increasing++
+                        } else if (lineInt[k - 1] > lineInt[k] && diference <= 3) {
+                            decreasing++
+                        }
+                    }
+                    if (increasing == lineInt.size - 1 || decreasing == lineInt.size - 1) {
+                        answer++
+                        break
+                    }
+                    increasing = 0
+                    decreasing = 0
+                    lineInt.add(j, value)
+                }
+            }
+        }
+        return answer
+    }
+
     val input = readInput("Day02")
     part1(input).println()
+    part2(input).println()
 
 }
